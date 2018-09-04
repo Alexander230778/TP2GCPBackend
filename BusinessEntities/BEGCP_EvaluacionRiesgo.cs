92000bfc1bec0ea4e0f273df3f43fdd0e99c75ac
ALP <alexander112330@gmail.com>
2018-09-04 13:10:19 -0500

    EvaluacionRiesgo Corrección

--- BusinessEntities/BEGCP_EvaluacionRiesgo.cs
+++ BusinessEntities/BEGCP_EvaluacionRiesgo.cs
@@ -9,17 +9,20 @@ namespace BusinessEntities
     public class BEGCP_EvaluacionRiesgo
     {
         public int acci { get; set; }
+        public int evr_Codigo { get; set; }
         public int rfc_Codigo { get; set; }
         public bool evr_Requiere { get; set; }
         public int pri_Codigo { get; set; }
+        public string pri_Descripcion { get; set; }
         public string evr_Observacion { get; set; }
-        public int evr_Codigo { get; set; }
+        public DateTime evr_FechaEnvio { get; set; }
         public int esr_Codigo { get; set; }
+        public string esr_Descripcion { get; set; }
+        public DateTime evr_FechaRespuesta { get; set; }
         public string evr_Informe { get; set; }
         public int imp_Codigo { get; set; }
+        public string imp_Descripcion { get; set; }
         public string evr_Adjunto { get; set; }
-        public DateTime evr_FechaEnvio { get; set; }
-        public DateTime evr_FechaRespuesta { get; set; }
         public bool evr_LineaBase { get; set; }
         public bool evr_Presupuesto { get; set; }
     }
